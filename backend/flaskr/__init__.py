9cbaa1341b5a3127c87b8914f809c08819e5b285
Scott <scott@scottrobertsmith.com>
2019-12-01 14:23:48 -0800

    Fixed pip8 formatting

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,74 +8,63 @@ from models import setup_db
 
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  app.register_blueprint(trivia_api)
-  setup_db(app)
-  
-  
-  CORS(app)
-  
-  # CORS Headers 
-  @app.after_request
-  def after_request(response):
-      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-      response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-      response.headers.add('Access-Control-Allow-Origin', '*')
-      return response
+    # create and configure the app
+    app = Flask(__name__)
+    app.register_blueprint(trivia_api)
+    setup_db(app)
+    CORS(app)
 
-  
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-  #-----------------------------------------------------------------------------
-  #  error handlers
-  #-----------------------------------------------------------------------------
+    # ------------------------------------------------------------------------#
+    #  error handlers
+    # ------------------------------------------------------------------------#
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad Request"
+        }), 400
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Resource Not Found"
+        }), 404
 
-  @app.errorhandler(400)
-  def bad_request(error):
-      return jsonify({
-          "success": False, 
-          "error": 400,
-          "message": "Bad Request"
-      }), 400
+    @app.errorhandler(405)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "Method Not Allowed"
+        }), 405
 
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable"
+        }), 422
 
+    @app.errorhandler(500)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Internal Server Error"
+        }), 500
 
-  @app.errorhandler(404)
-  def not_found(error):
-      return jsonify({
-          "success": False, 
-          "error": 404,
-          "message": "Resource Not Found"
-      }), 404
-
-
-  @app.errorhandler(405)
-  def not_found(error):
-      return jsonify({
-          "success": False, 
-          "error": 405,
-          "message": "Method Not Allowed"
-      }), 405
-
-
-  @app.errorhandler(422)
-  def unprocessable(error):
-      return jsonify({
-          "success": False, 
-          "error": 422,
-          "message": "Unprocessable"
-      }), 422
-
-  @app.errorhandler(500)
-  def unprocessable(error):
-      return jsonify({
-          "success": False, 
-          "error": 500,
-          "message": "Internal Server Error"
-      }), 500
-
- 
-  return app
-
-    
+    return app
